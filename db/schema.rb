c9609027782ea46ef8ef4e52f038c9f8764d7761
Alex Little <alittle1092@gmail.com>
2020-05-06 15:19:54 +1000

    Deleted doctor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_033816) do
+ActiveRecord::Schema.define(version: 2020_05_06_051725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "doctors", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone"
-    t.index ["email"], name: "index_doctors_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
-  end
-
   create_table "scripts", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "doctor_id", null: false
     t.text "details"
     t.date "expiration"
-    t.date "date_printed"
+    t.string "doctor"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["doctor_id"], name: "index_scripts_on_doctor_id"
     t.index ["user_id"], name: "index_scripts_on_user_id"
   end
 
@@ -54,6 +37,5 @@ ActiveRecord::Schema.define(version: 2020_05_06_033816) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "scripts", "doctors"
   add_foreign_key "scripts", "users"
 end
