5724883bd427332c7faf60f2c41130bb7cd2cc22
Vijay <vijaypabial@yahoo.com>
2020-05-07 09:55:04 +1000

    Added about page, image added, some lorem ipsum

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :scripts
   devise_for :users
   root 'pages#index'
+  get "pages/about", to: "pages#about", as: "about"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,21 @@ ActiveRecord::Schema.define(version: 2020_05_06_071014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "doctors", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.index ["email"], name: "index_doctors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
@@ -43,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_071014) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
